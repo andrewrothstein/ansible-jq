21a3ffb89ac6de1164223ab59b90a30793d972d2
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-07-29 12:29:56 -0400

    clean up checksums. add urls.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env sh
-VER=1.6
+set -e
 DIR=~/Downloads
-MIRROR=https://github.com/stedolan/jq/releases/download/jq-$VER
+MIRROR=https://github.com/stedolan/jq/releases/download
 
 dl()
 {
-    PLATFORM=$1
-    SUFFIX=${2:-}
-    wget -O $DIR/jq-$VER-$PLATFORM$SUFFIX $MIRROR/jq-$PLATFORM
+    ver=$1
+    platform=$2
+    dotexe=${3:-}
+    lfile=$DIR/jq-${ver}-${platform}${dotexe}
+    url=${MIRROR}/jq-${ver}/jq-${platform}${dotexe}
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
 }
 
-dl linux32
-dl linux64
-dl osx-amd64
-dl win32 .exe
-dl win64 .exe
-sha256sum $DIR/jq-$VER-*
+dl_ver() {
+    ver=$1
+    printf "  '%s'\n" $ver
+    dl $ver linux32
+    dl $ver linux64
+    dl $ver osx-amd64
+    dl $ver win32 .exe
+    dl $ver win64 .exe
 
+}
+
+dl_ver ${1:-1.6}
